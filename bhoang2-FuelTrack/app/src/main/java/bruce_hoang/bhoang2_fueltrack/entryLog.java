d553812a52e0c227b817d12927600923b2092469
b-hoang <bhoang2@cs.ualberta.ca>
2016-01-28 14:01:10 -0700

    minor changes

--- bhoang2-FuelTrack/app/src/main/java/bruce_hoang/bhoang2_fueltrack/entryLog.java
+++ bhoang2-FuelTrack/app/src/main/java/bruce_hoang/bhoang2_fueltrack/entryLog.java
@@ -4,6 +4,22 @@ package bruce_hoang.bhoang2_fueltrack;
  * Created by Bruce-PC on 1/27/2016.
  */
 public class entryLog {
-    public String date, station, fuelGrade;
-    public float odometer, fuelAmount, fuelUnitCost, fuelCost;
+    private String date, station, fuelGrade;
+    private float odometer, fuelAmount, fuelUnitCost, fuelCost;
+
+    private entryLog(){
+
+    }
+
+    public entryLog(String date, String station, String fuelGrade, float odometer,
+                    float fuelAmount, float fuelUnitCost, float fuelCost){
+        this.date = date;
+        this.station = station;
+        this.odometer = odometer;
+        this.fuelGrade = fuelGrade;;
+        this.fuelAmount = fuelAmount;
+        this.fuelUnitCost = fuelUnitCost;
+        this.fuelCost = fuelCost;
+
+    }
 }
--- bhoang2-FuelTrack/app/src/main/java/bruce_hoang/bhoang2_fueltrack/newEntryActivity.java
+++ bhoang2-FuelTrack/app/src/main/java/bruce_hoang/bhoang2_fueltrack/newEntryActivity.java
@@ -1,7 +1,10 @@
 package bruce_hoang.bhoang2_fueltrack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class newEntryActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class newEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_entry);
+
+        Button cancelButton = (Button) findViewById(R.id.cancel);
+//        Button newEntryButton = (Button) findViewById(R.id.new_entry);
+
+        /*newEntryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonAddNewEntry(v);
+            }
+        });*/
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
+
 }
